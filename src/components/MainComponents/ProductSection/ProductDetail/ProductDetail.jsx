49237a05c158dd83b76a4502bf70149dd2aa4e33
RamiroPerez <ramiperez1997@hotmail.com>
2023-01-05 16:56:18 -0300

    en proceso de rutas anidadas

---
+++ src/components/MainComponents/ProductSection/ProductDetail/ProductDetail.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export const ProductDetail = () => {
+  return (
+    <>
+      <main>
+        Esto es un detalle del producto
+      </main>
+    </>
+  )
+}
--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -3,6 +3,7 @@ import {Route,Routes as ReactDomRoutes} from 'react-router-dom';
 import {Home} from '../pages/Home/Home.jsx';
 import {Products} from '../pages/Products/Products.jsx';
 import {Error404} from '../components/Error404/Error404.jsx';
+import {ProductDetail} from '../components/MainComponents/ProductSection/ProductDetail/ProductDetail.jsx';
 
 export const Routes = () => {
   return (
@@ -10,6 +11,10 @@ export const Routes = () => {
       {/* Home: Vista de la portada + algunas cositas que se agreguen (ventajas de comprar en Perfarm) */}
       <Route path='/' element={<Home />} />
       {/* Products: Vista de los productos + filtros de busqueda*/}
+      <Route path='/productos'>
+        <Route index element={<Products />} />
+        <Route path=':productos' element={<ProductDetail />} />
+      </Route>
       <Route path='/productos' element={<Products />} />
       {/* ruta del error 404*/}
       <Route path='*' element={<Error404 />} />
